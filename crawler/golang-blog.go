e8d5ecec4c8b0c3cd823ca7175d841d478094c37
Tong Sun <suntong@cpan.org>
2016-05-26 14:24:46 -0400

    - [+] add golang-blog.go, from https://www.progville.com/go/goquery-jquery-html-golang/

---
+++ crawler/golang-blog.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	doc, err := goquery.NewDocument("https://blog.golang.org")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".article").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("h3").Text()
+		link, _ := s.Find("h3 a").Attr("href")
+		fmt.Printf("%d) %s - %s\n", i+1, title, link)
+	})
+}
