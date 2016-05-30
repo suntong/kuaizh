e264e37f12ccf4f571e0de3c1305ce60199ab401
Tong Sun <suntong@cpan.org>
2016-05-30 17:33:54 -0400

    - [+] add scrape_kijiji_car.go

---
+++ crawler/goscrape/scrape_kijiji_car.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/andrew-d/goscrape"
+	"github.com/andrew-d/goscrape/extract"
+	"github.com/andrew-d/goscrape/paginate"
+)
+
+func main() {
+	config := &scrape.ScrapeConfig{
+		DividePage: scrape.DividePageBySelector("div.info-container"),
+
+		Pieces: []scrape.Piece{
+			{Name: "FullInfo", Selector: ".", Extractor: extract.OuterHtml{}},
+			{Name: "Price", Selector: "div.price", Extractor: extract.Text{}},
+			// {Name: "Id", Selector: "div.title>a.attr('href')",
+			// 	Extractor: extract.Regex{Regex: regexp.MustCompile(`.*/(\d+)$`)}},
+			{Name: "Title", Selector: "div.title", Extractor: extract.Text{}},
+			{Name: "Description", Selector: "div.description", Extractor: extract.OuterHtml{}},
+			//{Name: "", Selector: "div.", Extractor: extract.Text{}},
+		},
+
+		Paginator: paginate.BySelector("a[title='Next']", "href"),
+	}
+
+	scraper, err := scrape.New(config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating scraper: %s\n", err)
+		os.Exit(1)
+	}
+
+	results, err := scraper.ScrapeWithOpts(
+		"http://www.kijiji.ca/b-cars-trucks/gta-greater-toronto-area/gps/k0c174l1700272?ad=offering&price=__18000&kilometers=__90000&transmission=2&for-sale-by=ownr",
+		scrape.ScrapeOptions{MaxPages: 5},
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error scraping: %s\n", err)
+		os.Exit(1)
+	}
+
+	json.NewEncoder(os.Stdout).Encode(results)
+}
