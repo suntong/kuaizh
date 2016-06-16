0a8679ddf474135a19832d8204a427a9aa2fda31
Tong Sun <suntong@cpan.org>
2016-06-15 23:29:03 -0400

    - [+] add kijiji goquery op demo

---
+++ crawler/kijiji.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+/*
+
+<div class="info-container">
+  <div class="price">$14,900.00</div>
+
+  <div class="title">
+    <a href="/v-cars-trucks/mississauga-peel-region/2014-dodge-journey-sxt-bluetooth-gps-nav-heated-backup-sensor/1173567915?src=topAdSearch" class="title enable-search-navigation-flag ">
+    2014 Dodge Journey SXT Bluetooth GPS Nav Heated Backup Sensor</a>
+  </div>
+
+  <div class="distance">
+  </div>
+
+  <div class="location">
+    Mississauga / Peel Region<span class="date-posted">13/06/2016</span>
+  </div>
+
+  <div class="description">
+    I am selling my 2014 Dodge Journey SXT... <div class="details">
+    47000km | Automatic</div>
+  </div>
+</div>
+
+*/
+
+func main() {
+	doc, _ := goquery.NewDocument("http://www.kijiji.ca/b-cars-trucks/gta-greater-toronto-area/gps/k0c174l1700272?ad=offering&price=__18000&kilometers=__90000&transmission=2&for-sale-by=ownr")
+	doc.Find("div.info-container").Each(func(_ int, s *goquery.Selection) {
+		title := s.Find("div.title")
+		location := s.Find("div.location")
+		description := s.Find("div.description")
+		titleHtml, _ := title.Html()
+		locationHtml, _ := location.Html()
+		descriptionHtml, _ := description.Html()
+		fmt.Printf("%v\n%v\n%v\n\n=>\n", titleHtml, locationHtml, descriptionHtml)
+		titleHtml, _ = title.RemoveFiltered("a").Html()
+		locationHtml, _ = location.RemoveClass("date-posted").Html()
+		descriptionHtml, _ = description.Find("div").Remove().Html()
+		fmt.Printf("%v\n%v\n%v\n\n---\n", titleHtml, locationHtml, descriptionHtml)
+	})
+}
