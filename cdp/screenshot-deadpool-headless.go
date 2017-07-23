e69a3282e5ba13ffb6fa296db3d214ea32836fa9
Tong Sun <suntong@cpan.org>
2017-07-23 17:46:07 -0400

    - [+] add cdp/, knq/chromedp

---
+++ cdp/screenshot-deadpool-headless.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"time"
+
+	cdp "github.com/knq/chromedp"
+	"github.com/knq/chromedp/client"
+)
+
+func main() {
+	var err error
+
+	// create context
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// create chrome instance
+	//c, err := cdp.New(ctxt)
+	// create chrome-headless instance
+	c, err := cdp.New(ctxt, cdp.WithTargets(client.New().WatchPageTargets(ctxt)), cdp.WithErrorf(log.Printf))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run task list
+	var buf []byte
+	err = c.Run(ctxt, screenshot(`http://www.cmore.se/film/3643033-deadpool`, `#main-wrapper`, &buf))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// shutdown chrome
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// wait for chrome to finish
+	err = c.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("deadpool.png", buf, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func screenshot(urlstr, sel string, res *[]byte) cdp.Tasks {
+	return cdp.Tasks{
+		cdp.Navigate(urlstr),
+		cdp.Sleep(2 * time.Second),
+		cdp.WaitVisible(sel, cdp.ByID),
+		cdp.Screenshot(sel, res, cdp.ByID),
+	}
+}
