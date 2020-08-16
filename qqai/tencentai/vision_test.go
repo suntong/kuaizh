34a6ae83a43ff5a91e731ff6c27631ac9b62c7ff
Tong Sun <suntong@cpan.org>
2020-08-16 11:37:45 -0400

    - [!] make it works

--- qqai/tencentai/vision_test.go
+++ qqai/tencentai/vision_test.go
@@ -4,21 +4,33 @@ package tencentai
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 func TestVisionImgFileToText(t *testing.T) {
 
-	client := NewAiClient("your-appid", "your-appkey")
+	client := NewAiClient(os.Getenv("QQ_AI_APPID"), os.Getenv("QQ_AI_APPKEY"))
 
 	err, vtt := client.VisionImgFileToText("./fm_big.jpg")
 	if err != nil {
-		t.Fatalf("Expect success but return:", err)
+		t.Fatalf("Expect success but returned: %v", err)
 	} else {
 		if vtt.Ret != 0 {
-			t.Fatalf("Expect ret is 0 but%v msg:%s", vtt.Ret, vtt.Msg)
+			t.Fatalf("Expect ret is 0 but%v msg:%s:", vtt.Ret, vtt.Msg)
 		} else {
 			fmt.Println("text:", vtt.Data.Text)
 		}
 	}
 }
+
+/*
+
+export QQ_AI_APPID=... QQ_AI_APPKEY=... TZ='Asia/Shanghai'
+
+$ go test
+text: 海面上一艘白色的船.
+PASS
+ok  	tencentai	2.554s
+
+*/
